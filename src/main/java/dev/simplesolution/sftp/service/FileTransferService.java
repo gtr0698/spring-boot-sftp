38efd0db5187286d876e9c92c0991a581ae4cb04
Guilherme <guilherme_taborda@live.com>
2022-09-21 10:54:40 -0300

    Primeiro Commit SFTP

---
+++ src/main/java/dev/simplesolution/sftp/service/FileTransferService.java
@@ -0,0 +1,8 @@
+package dev.simplesolution.sftp.service;
+
+public interface FileTransferService {
+
+    boolean uploadFile(String localFilePath, String remoteFilePath);
+
+    boolean downloadFile(String localFilePath, String remoteFilePath);
+}
